e9e72479e23c5e25dd734ac51c9c4d569caac42d
imakhlaq <imakhlaqXD@gmail.com>
2022-09-01 20:39:46 +0530

    Edge

---
+++ Learning/Random/factorsOfn.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+
+
+// only checking for n/2 because numbers only corres in n/2
+//and for n th factor taking count =1
+int count=1;
+    for(int i=1;i<n/2;i++){
+        if(n%i ==0){
+            count ++;
+            cout<<i<<" ";
+        }
+    }
+    cout<<count;
+
+//even btter approach
+
+// int count=1;
+//     for(int i=1;i*i<n;i++){
+//         if(n%i ==0){
+//             count ++;
+//             cout<<i<<" ";
+//         }
+//     }
+//     cout<<count;
+
+
+    return 0;
+}
