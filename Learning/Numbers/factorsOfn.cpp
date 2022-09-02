f59075ec50f9845bee6a0e20540dbc07ff678fe4
imakhlaq <imakhlaqXD@gmail.com>
2022-09-02 09:46:02 +0530

    prime

--- Learning/Numbers/factorsOfn.cpp
+++ Learning/Numbers/factorsOfn.cpp
@@ -11,7 +11,7 @@ int main(){
     cout<<"enter the number : ";
     cin>>n;
 
-
+//===============for factor
 // only checking for n/2 because numbers only corres in n/2
 //and for n th factor taking count =1
 // int count=1;
@@ -23,21 +23,47 @@ int main(){
 //     }
 //     cout<<endl<<count;
 
-//even btter approach
+//even btter approach for factor
+// int count=0;
+//     for(int i=1;i*i<=n;i++){
+//         if(n%i ==0){
+//             count++;
+//             if(i!= n/i){
+//                 count++;
+//             }
+//         }
+//     }
+//     cout<<endl<<count;
+
+//================ prime or not
+// for(int i=2;i<n;i++){
+//     if(n%i==0 ){
+//         cout<<"not prime";
+//         break;
+//     }
+// }
+//     cout<<"prime";
+
+// better solution for prime
 int count=0;
-    for(int i=1;i*i<=n;i++){
-        if(n%i ==0){
-            
-           
-            if(i!=n/i){
-                count++;
-                cout<<i<<" ";
+for(int i=1;i*i<=n;i++){
+    if(i%n==0){
 
-            }
-        }
+        count++;
+
     }
-    cout<<endl<<count;
+    if(i != n/i){
+        count++;
+    }
 
+}
+if(count==2){
+    cout<<"prime";
+
+}
+else{
+    cout<<"not prime";
+}
 
 
     return 0;
