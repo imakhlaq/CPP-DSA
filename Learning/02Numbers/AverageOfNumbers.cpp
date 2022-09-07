0c35d68ab062b3574619cb2268e4366d2651ded8
imakhlaq <imakhlaqXD@gmail.com>
2022-09-07 11:43:54 +0530

    average of numbers

---
+++ Learning/02Numbers/AverageOfNumbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+       cout<<"Average OF :";
+
+        int n;
+        int num;
+        int avg=0;
+        cin>>n;
+        for(int i=1;i<=n;i++){
+
+            cout<<"Enter the number : "<<endl;
+            cin>>num;
+            avg+=num;
+
+        }
+
+        cout<<avg/n;
+
+
+
+
+        return 0;
+    }
