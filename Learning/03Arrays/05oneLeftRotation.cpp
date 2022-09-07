b17ff060ed6a612d4f6145b930d7fddabb900b2a
imakhlaq <imakhlaqXD@gmail.com>
2022-09-07 18:33:17 +0530

    one left rotation

---
+++ Learning/03Arrays/05oneLeftRotation.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void leftRotate(int arr[],int n){
+
+int temp=arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+
+    }
+    arr[n-1]=temp;
+}
+
+
+int main(){
+
+
+        cout<<"Size of Array :";
+        int n;
+        cin>>n;
+
+        int arr[n];
+
+          for(int i=0;i<n;i++){
+            cout<<"enter the elements "<<i<<" : ";
+            cin>>arr[i];
+          }
+
+          leftRotate(arr,n);
+
+            for(int x:arr){
+                cout<<endl<<x;
+            }
+
+
+
+        return 0;
+    }
