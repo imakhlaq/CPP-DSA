af91634fb074e0dd20bb7b1803256a581178a214
imakhlaq <imakhlaqXD@gmail.com>
2022-09-07 13:38:48 +0530

    array

---
+++ Learning/03Arrays/01ArrayBasics.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+
+
+        cout<<"Enter the size of array :";
+
+        int n;//taking the size of array from user;
+        cin>>n;
+        int arr[n];//while declaring and array You can use constants or varibles
+
+        //taking elements from the user
+        for (int i = 0; i < n; i++)
+        {
+
+            cout<<"Enter the element "<<i+1<<" : ";
+            cin>>arr[i];
+        }
+        
+
+        //displaying array element
+
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+
+
+
+
+        return 0;
+    }
+
+    //you can make an array of any type and its store in contigious manner means an array of int (int arr[3]) will be sotred with the gap of 4 bytes because the one int takes 1 byte to store the value.
+
+   // and array index star from zero (arr[o]) to store and retrive use 0 index . array are fixed in nature one you inisilized the array you cant change it . 
+   // their are differnt ways to inisilize the array
+   // 1== int arr[varible/constant];
+   // the push the value inside it
+   //2== int arr[]={2,3,4,2,1}
+   //when you create the array you can inisilize it and in this you can ommit the the value in side []. cpp will figure out accorfing the elemts in { }. not in 2D[][](you have to specify one ). in 3D(you have to specify 2last [][][])
+   // dynamic memory allocatiom
+   //int arr[]=new int 
