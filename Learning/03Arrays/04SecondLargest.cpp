22caaa7b1e2a388f5891cd9925fc5068153c2603
imakhlaq <imakhlaqXD@gmail.com>
2022-09-07 18:20:59 +0530

    second largest

---
+++ Learning/03Arrays/04SecondLargest.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int secondLargest(int arr[],int n){
+        int second=-1;
+        int largest=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+                second=largest;
+                largest=arr[i];
+
+            }
+            if(second<arr[i] && arr[i]!=largest){
+                second=arr[i];
+            }
+
+        }
+        return second;
+    }
+
+
+int main(){
+
+
+    cout<<"Size of Array :";
+        int n;
+        cin>>n;
+
+        int arr[n];
+
+          for(int i=0;i<n;i++){
+            cout<<"enter the elements "<<i<<" : ";
+            cin>>arr[i];
+          }
+
+
+          int secondLargestElement=secondLargest(arr,n);
+    
+
+          cout<<endl<<secondLargestElement;
+
+
+
+
+        return 0;
+    }
