a3b1382f5a7800539f02abffdd648ade7c09f4b8
imakhlaq <imakhlaqXD@gmail.com>
2022-09-12 11:33:40 +0530

    did spiral matrix

--- Learning/04-2DArrays/10SpiralMatrix.cpp
+++ Learning/04-2DArrays/10SpiralMatrix.cpp
@@ -3,19 +3,52 @@ using namespace std;
 
       vector<int> reverseTheMatrix(vector<vector<int>> mat){
 
-         int startrow=0;
-         int endrow=mat.size();
-         int startcol=0;
-         int endcol=mat[0].size();
+         int top=0;
+         int down=mat.size()-1;
+         int left=0;
+         int right=mat[0].size()-1;
             
-          int i=0;  //itrater
+          int dir=0;  //itrater
 
 
 
 
-            while(startrow<endrow && startcol<endcol){
+
+            while(top<=down && left<=right){
+
+                if(dir==0){
+                    for(int i=0;i<=right;i++){
+                        cout<<mat[top][i];
+                    }
+                    top++;
+                }
+
+                if(dir==1){
+                    for(int j=top;j<=down;j++){
+                        cout<<mat[j][right];
+
+                    }
+                    right--;
+                }
+
+               else if(dir==2){
+                    for(int k=right;k>=left;k--){
+                        cout<<mat[down][k];
+
+                    }
+                    down--;
+                }
 
 
+                else if(dir==3){
+                    for(int l=down;l<=top;l--){
+                        cout<<mat[l][left];
+                    }
+                    left++;
+                }
+                 
+
+                 dir=(dir+1)%4; 
 
 
                 
@@ -32,6 +65,7 @@ int main(){
      
         int row;
         int col;
+        cin>>row>> col;
 
         vector<vector<int>> mat;
 
@@ -45,6 +79,18 @@ int main(){
             }
             mat.push_back(temp);
         }
+
+
+        for(int i=0;i<mat.size();i++){
+        for(int j=0;j<mat[0].size();j++){
+            cout<<mat[i][j]<<" ";
+                
+
+            }
+            cout<<endl;
+        }
+
+        cout<<endl;
 
             reverseTheMatrix(mat);
 
---
+++ Learning/05String/02ConvertUpperLOwercase.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void toUdpper(string str){
+    
+    int index=str.length();
+    
+    int i=0;
+    while(i<index){
+        
+            if(str[i]>=97 && str[i]<=122){   //for lower if(str[i]<=65 && str[i]>=90){str[i]+=32;}
+              str[i]-=32;
+            }
+            
+        
+        i++;
+    }
+
+    isalpha(str[0]); 
+
+    cout<<str;
+    
+}
+
+string caseChange(string str){
+
+    int l=str.length();
+    int i=0;
+    while(i<l){
+        if(str[i]>=65 && str[i]<=90){
+            str[i]+=32;
+        }
+        else{
+            str[i]-=32;
+        }
+        i++;
+
+    }
+    return str;
+}
+
+
+
+int main(){
+
+
+    string str="aSKdadtdSf";
+    string str1=str;
+
+        cout<<str1;
+   
+
+
+   //cout<<endl<<caseChange(str);
+
+
+
+
+        return 0;
+}
+      
