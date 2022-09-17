9ff9fbe4a6bbda5f05f694a0100ff75344a12944
imakhlaq <imakhlaqXD@gmail.com>
2022-09-17 13:15:56 +0530

    reverse sub array

---
+++ DSA/Arrays/ReverseSubArray.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//sub array is ==> contigious part of array
+
+    void reverseTheSub(int arr[], int start,int end){
+
+            while(start<end){
+                swap(arr[end],arr[start]);
+                start++;
+                end--;
+            }
+
+    }
+
+
+int main(){
+
+    int arr[]={3,2,7,9,12,6,8,4,1};
+
+
+
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int start,end;
+    cin>>start>>end;
+    //taking input of sub array to be reverse and passing it to the function
+    reverseTheSub(arr,start,end);
+
+
+    for(int x: arr){
+        cout<<x<<" ";
+    }
+
+         
+
+
+
+
+        return 0;
+    }
