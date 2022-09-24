c5d46984fcde68c48413113fd5129b8ffc61e189
imakhlaq <imakhlaqXD@gmail.com>
2022-09-24 18:50:21 +0530

    next permutatio

--- DSA/Arrays/11_JustGreaterPermetation.cpp
+++ DSA/Arrays/11_JustGreaterPermetation.cpp
@@ -14,37 +14,80 @@ using namespace std;
 
         void nextPermituation(int arr[] ,int n){
 
-            int i;
+            // int i;
             
 
-            for(i=n-2;i>=0;i--){
-                if(arr[i]<arr[i+1]){
+            // for(i=n-2;i>=0;i--){
+            //     if(arr[i]<arr[i+1]){
                     
-                    break;
+            //         break;
                     
-                }
-            }
-            //is i value is zero the will run run it will stop when i is -1
-                int j=n-1;
-                while(j>i){
-                    if(arr[j]>arr[i]){
-                        break;
-                    }
-                    j--;
-                }
+            //     }
+            // }
+            // //is i value is zero the will run run it will stop when i is -1
+            //     int j=n-1;
+            //     while(j>i){
+            //         if(arr[j]>arr[i]){
+            //             break;
+            //         }
+            //         j--;
+            //     }
 
-                swap(arr[i],arr[j]);
+            //     swap(arr[i],arr[j]);
+            //     cout<<i;
                
-            reverse1(arr,i+1,j);       
+            // reverse1(arr,i+1,j);       
             
+             int i;
+        
+        for(i=n-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                break;
+
+            }
+
+        }
+        
+               
+              int j=n-1;
+                if(i>=0){
             
+            while(j>i){
+                if(arr[j]>arr[i]){
+                    break;
+
+                }
+                j--;
+
+            }
+            
+            swap(arr[i],arr[j]);
+            
+            }
+
+
+            for(int i=0;i<n;i++){
+                cout<<arr[i];
+            }
+            cout<<endl;
+
+        
+        
+        if(i>=0){
+        reverse(arr+i+1,arr+n);
+        }
+        
+        else{
+            
+            reverse1(arr, i+1,n-1);
+        }
         }
 
 
 int main(){
 
 
-        int arr[]={ 1,2,3 };
+        int arr[]={ 2,3,1 };
 
         int n=sizeof(arr)/sizeof(arr[0]);
 
