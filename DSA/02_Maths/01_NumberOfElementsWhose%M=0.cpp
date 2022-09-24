662399660a9cc9361d12b99a30e46d63afc77061
imakhlaq <imakhlaqXD@gmail.com>
2022-09-24 22:29:55 +0530

    pair of elements when added and mod by m is equal 0

---
+++ DSA/02_Maths/01_NumberOfElementsWhose%M=0.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+pair of sub array is when % by m is ==0;
+*/
+
+int elmentsModm0(int arr[],int n,int m){
+
+    int count [m]={0};
+    for(int i=0;i<n;i++){
+
+        int rem=arr[i]%m;
+
+        count[rem]++;
+    }
+
+    int ans=0;
+
+    ans+=(count[0]*count[0]-1)/2;
+
+
+    for(int i=1;i<=m/2;i++){
+        ans+=count[i]*count[m-i];
+
+    }
+    if(m%2==0){
+        ans+=(count[m/2]*count[m/2]+1)/2;
+
+
+    }
+
+    return ans;
+    
+}
+
+
+int main(){
+
+
+        int arr[]={7,2,5,4,6,3,13,26,14,18,15,30,35,10,40};
+
+
+       int n=sizeof(arr)/sizeof(arr[0]);
+       int m=10;
+
+      int res=elmentsModm0(arr,n,m);
+
+      cout<<res;
+     
+
+
+
+
+
+
+
+        return 0;
+    }
