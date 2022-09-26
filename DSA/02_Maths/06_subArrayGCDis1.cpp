8e1ed63999ea67f2db26fc47939738c086a40315
imakhlaq <imakhlaqXD@gmail.com>
2022-09-26 13:33:45 +0530

    sub array gcd 1

---
+++ DSA/02_Maths/06_subArrayGCDis1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+if then exits a sub array gcd==1;
+
+check for whole array if any sub array gcd ==1 the yes(break where you find) else no
+*/
+
+
+int main(){
+
+
+        
+
+
+
+
+        return 0;
+    }
