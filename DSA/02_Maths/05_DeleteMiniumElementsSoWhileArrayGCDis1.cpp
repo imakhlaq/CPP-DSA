4f25799284234ae686f7ac7534680017ba333305
imakhlaq <imakhlaqXD@gmail.com>
2022-09-26 13:26:29 +0530

    minimun element to remove to be whole sub array gcd 1

---
+++ DSA/02_Maths/05_DeleteMiniumElementsSoWhileArrayGCDis1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+Q minimum element we should remove to get array GCD is 1
+
+if the whole array gcd is >1 than no matter how many element we remove it will nerver be less than 1 (it might increase).
+
+if initial Gcd =1 yes 0element
+               !=1 it can nver be possible
+
+
+
+
+*/
+
+int main(){
+
+
+        
+
+
+
+
+        return 0;
+    }
