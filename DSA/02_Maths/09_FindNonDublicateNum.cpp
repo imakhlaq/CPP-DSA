c127199d18090250f136b365f0614e0e685a6695
imakhlaq <imakhlaqXD@gmail.com>
2022-09-28 16:16:06 +0530

    find non dublicate num using XOR

--- DSA/02_Maths/09_FindNonDublicateNum.cpp
+++ DSA/02_Maths/09_FindNonDublicateNum.cpp
@@ -12,19 +12,39 @@ xor
 
 
 
+            // int nonDublicate(vector<int>vec){
+            //     int num;
+
+            //     for(int i=0;i<vec.size();i++){
+            //         int j;
+            //         for(j=0;j<vec.size();j++){
+            //             if(i != j && vec[i] == vec[j]){          
+            //             break;
+            //             }
+            //         }
+            //           if(j==vec.size()){
+            //              num=vec[i];
+            //         }
+            //     }
+
+            //     return num;
+            // }
+
+
+/*
+XOR property
+x^x=0;
+x^0=x;
+*/
             int nonDublicate(vector<int>vec){
-                int num;
 
-                for(int i=0;i<vec.size();i++){
-                    for(int j=0;j<vec.size();j++){
-                        if(i != j && vec[i] == vec[j]){
-                            num=vec[i];
+                    int n=vec[0];
+                for(int i=1;i<vec.size();i++){
 
-                        }
-                    }
+                    n=n^vec[i];
+                    
                 }
-
-                return num;
+                return n;
             }
 
 
@@ -33,6 +53,11 @@ int main(){
 
 
     vector<int> vec {1,5,6,1,7,5,6};
+
+
+
+    //O(n2) solution
+    // int n=nonDublicate(vec);
 
     int n=nonDublicate(vec);
 
