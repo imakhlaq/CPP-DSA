052f6c5c21c0de669b53bff7366cdb0bb5b93f49
imakhlaq <imakhlaqXD@gmail.com>
2022-09-29 20:51:18 +0530

    ith bitof number

---
+++ DSA/02_Maths/11_PowerOfTo.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+            void powerOf(int n,int k=5){
+                if(k==1){
+                    return;
+                }
+                powerOf(n,k=k-1);
+                int p=k<<n;
+                cout<<p<<" ";
+            }
+
+
+
+int main(){
+
+
+        int n=2;
+        
+
+      powerOf(n);
+
+        return 0;
+    }
---
+++ DSA/02_Maths/12_ithBitIsSetOrNot.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+        bool bitCheck(int n,int ith){
+            int p=n>>ith;
+            if(p&1){
+                return true;
+            }
+            return false;
+        }
+
+
+int main(){
+
+
+        int n=4;
+        int ith=0;
+
+        bool a=bitCheck(n,ith);
+
+        cout<<a;
+
+
+
+
+        return 0;
+    }
