ee338185a3fb5a44a0d3a841a854240dce472fb4
imakhlaq <imakhlaqXD@gmail.com>
2022-10-01 19:51:42 +0530

    number of possible subsets of number instruction

--- Contest/05_KSubsetInAnArray.cpp
+++ Contest/05_KSubsetInAnArray.cpp
@@ -15,9 +15,27 @@ using namespace std;
       //subset is you can choose any number from array any amount but you cant choose a number twice
         bool posibleKSubset(vector<int>nums,int k){
                 int n=nums.size();
+                //making every possible combination using truth table thig
+                /*
+                formula for this is 2^n
+                for[1,2,4][
+                   0== 0 0 0
+                   1== 0 0 1
+                   2== 0 1 0
+                   3== 0 1 1
+                   4== 1 0 0
+                   5== 1 0 1
+                   6== 1 1 0
+                   7== 1 1 1
+
+                ]
+                considering if any number is included it will be one in truth table
+
+                */
             for(int i=0;i<pow(2,n);i++){
                 
                 int sum=0;
+                //iterating the truth table cand checking for included number and adding it to sum
                 for(int j=0;j<nums.size();j++){
 
                     if(checkbit(i,j)){
@@ -26,7 +44,7 @@ using namespace std;
                         
                     }
                 }
-               
+                // and in last checking if the sum ==k
                 if(sum==k){
                     return true;
                 }
