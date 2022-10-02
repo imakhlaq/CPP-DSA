b4890237bd3c32a89e746ba432c72f1b3c6da28f
imakhlaq <imakhlaqXD@gmail.com>
2022-10-02 14:21:19 +0530

    unique element in three repeating

---
+++ Contest/05_findUniquIn3Repeating3nums.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+        int uniqueEle(vector<int>nums){
+
+
+
+            
+        }
+
+
+
+int main(){
+
+
+         vector<int>nums ={5,7,5,4,7,11,11,9,11,7,5,4,4};
+
+
+         int n=uniqueEle(nums);
+
+        return 0;
+    }
