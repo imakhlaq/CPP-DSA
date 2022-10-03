10a0a19c57809cda45b1b1700b08d7d356f2233f
imakhlaq <imakhlaqXD@gmail.com>
2022-10-04 04:20:06 +0530

    search in matrix in log n

--- DSA/01_Arrays/03_findANumberInSorted2D.cpp
+++ DSA/01_Arrays/03_findANumberInSorted2D.cpp
@@ -1,52 +1,69 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-        int searchele(int arr[][3],int n){
+        // int searchele(int arr[][5],int n){
 
 
-            int i=0;
-            int j=0;
+        //     int i=0;
+        //     int j=0;
 
-            while(i<3 &&j<3){
-                if((arr[i][j]+2)<n){
-                    i++;
-                }
+        //     while(i< &&j<3){
+        //         if((arr[i][j]+2)<n){
+        //             i++;
+        //         }
                 
-                else if((arr[i][j]+2)>n){
-                   for(int k=0;k<3;k++){
-                    if(arr[i][k]==n){
-                        return arr[i][k];
-                    }
+        //         else if((arr[i][j]+2)>n){
+        //            for(int k=0;k<3;k++){
+        //             if(arr[i][k]==n){
+        //                 return arr[i][k];
+        //             }
                     
                     
-                   }
-                }
-            }
+        //            }
+        //         }
+        //     }
 
-            return -1;
-        }
+        //     return -1;
+        // }    
+
+
+
+
+                    bool searchEle(vector<vector<int>>nums,int target){
+
+                        int row=nums.size();
+                        int col=nums[0].size();
+                    }
 
 
 int main(){
 
 
-        int arr[][3]={{1,2,3},
-                      {4,5,6},
-                      {7,8,9}  };
-
         
-        int n=5;
 
-        int num=searchele(arr,n);
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                cout<<arr[i][j]<<" ";
+        vector<vector<int>>nums={
+                      {1 ,4, 7, 11,15},
+                      {2 ,5, 8, 12,19},
+                      {3 ,6, 9, 16,22},
+                      {10,13,14,17,24},
+                      {18,21,23,26,30}  };
+
+
+
+            int target=5;
+
+
+        int num=searchEle(nums,target);
+
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums[0].size();j++){
+                cout<<nums[i][j]<<" ";
             }
             cout<<endl;
         }
 
-        cout<<endl<<num;
+       // cout<<endl<<num;
 
 
 
