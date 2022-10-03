d3e05f09130b01fe73a3f1eff9cb6c907faf3207
imakhlaq <imakhlaqXD@gmail.com>
2022-10-04 00:37:56 +0530

    GCD using recursion

---
+++ DSA/03_Recursion/05_GCDusingRecursion.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int GreatestCD(int num1,int num2){
+
+        if(num1<num2){
+            swap(num1,num2);
+        }
+
+        if(num2==0){
+            return num1;
+        }
+        GreatestCD(num1%num2,num2);
+        
+    }
+
+
+int main(){
+
+
+         int num1;
+         int num2;
+
+         cin>>num1>>num2;
+
+         int n=GreatestCD(num1,num2);
+
+            cout<<n;
+
+
+
+        return 0;
+    }
