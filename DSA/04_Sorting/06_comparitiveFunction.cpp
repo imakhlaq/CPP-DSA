7792b202487acb0b77cbf7c89e1ef69c110aa825
imakhlaq <imakhlaqXD@gmail.com>
2022-10-07 20:26:17 +0530

    sort function feature

---
+++ DSA/04_Sorting/06_comparitiveFunction.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    bool comp(char x,char  y){
+        if(x<y){
+            return true;
+        }
+        return false;
+    }
+
+        
+
+
+int main(){
+
+
+         string str="asbfcdfrsfg";
+
+        sort(str.begin(),str.end(),comp);
+
+        cout<<str;
+
+
+
+
+        return 0;
+    }  
