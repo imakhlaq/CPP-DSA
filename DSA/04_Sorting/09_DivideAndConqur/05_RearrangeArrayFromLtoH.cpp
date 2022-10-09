e397ce6e230818e2500792ffb5685e4e8d2ff53f
imakhlaq <imakhlaqXD@gmail.com>
2022-10-09 20:42:04 +0530

    re arrange from h to l

--- DSA/04_Sorting/09_DivideAndConqur/05_RearrangeArrayFromLtoH.cpp
+++ DSA/04_Sorting/09_DivideAndConqur/05_RearrangeArrayFromLtoH.cpp
@@ -5,7 +5,21 @@ using namespace std;
 rearrange the given array between the index L to H
 */
 
-void reArrangeFromLtoH(int arr[], int n, int l, int h) {}
+void reArrangeFromLtoH(int arr[], int n, int l, int h) {
+
+   int p1 = l + 1;
+   int p2 = h - 1;
+
+   while (p1 < p2) {
+      if (arr[p1] < arr[l]) {
+         p1++;
+      } else if (arr[p2] >= arr[l]) {
+         p2--;
+      } else {
+         swap(arr[p1], arr[p2]);
+      }
+   }
+}
 
 int main() {
 
