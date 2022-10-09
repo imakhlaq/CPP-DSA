02c64eb0524211d7d0687ad81ed62b15923ebfc0
imakhlaq <imakhlaqXD@gmail.com>
2022-10-09 12:29:19 +0530

    some fix

--- DSA/02_Maths/09_FindNonDublicateNum.cpp
+++ DSA/02_Maths/09_FindNonDublicateNum.cpp
@@ -10,24 +10,6 @@ find the element in the array that does not have dublicate
 xor
 */
 
-// int nonDublicate(vector<int>vec){
-//     int num;
-
-//     for(int i=0;i<vec.size();i++){
-//         int j;
-//         for(j=0;j<vec.size();j++){
-//             if(i != j && vec[i] == vec[j]){
-//             break;
-//             }
-//         }
-//           if(j==vec.size()){
-//              num=vec[i];
-//         }
-//     }
-
-//     return num;
-// }
-
 /*
 XOR property
 x^x=0;
