f1f4723811f17b535894f1add9c39854fa0370c2
imakhlaq <imakhlaqXD@gmail.com>
2022-10-22 18:07:21 +0530

    inversion count

--- DSA/04_Sorting/09_DivideAndConqur/07_InversionCount.cpp
+++ DSA/04_Sorting/09_DivideAndConqur/07_InversionCount.cpp
@@ -1,17 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-int mergeArrays(int arr[], int low, int mid, int high) {}
+int mergeArrays(int arr[], int low, int mid, int high) {
+   int p1 = low;
+   int p2 = mid;
+   vector<int> temp;
+   while (p1 < mid && p2 <= high) {
+      if (arr[p1] <= arr[p2]) {
+         temp.push_back(arr[p1]);
+         p1++;
+      } else {
+         temp.push_back(arr[p2]);
+         p2++;
+      }
+   }
+   while (p1 < mid) {
+      temp.push_back(arr[p1]);
+      p1++;
+   }
+   while (p2 <= high) {
+      temp.push_back(arr[p2]);
+      p2++;
+   }
+}
 
-void inversion(int arr[], int low, int high) {
+int inversion(int arr[], int low, int high) {
 
    if (low == high) {
-      return;
+      return 0;
    }
    int mid = (low + high) / 2;
-   inversion(arr, low, mid);
-   inversion(arr, mid + 1, high);
+   // in x and y calculating the inversion count in divided arrays
+   int x = inversion(arr, low, mid);
+   int y = inversion(arr, mid + 1, high);
+   // and in z calculating both the after combining
+   int z = mergeArrays(arr, low, mid + 1, high);
 
-   mergeArrays(arr, low, mid, high);
+   return x + y + z;
 }
 int main() {
 
