1b9c45c211194ad16961f261d10bd7f7599e6864
imakhlaq <imakhlaqXD@gmail.com>
2022-10-22 17:55:36 +0530

    inversion count

--- DSA/04_Sorting/09_DivideAndConqur/07_InversionCount.cpp
+++ DSA/04_Sorting/09_DivideAndConqur/07_InversionCount.cpp
@@ -1,15 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int mergeArrays(int arr[], int low, int mid, int high) {}
 
+void inversion(int arr[], int low, int high) {
 
+   if (low == high) {
+      return;
+   }
+   int mid = (low + high) / 2;
+   inversion(arr, low, mid);
+   inversion(arr, mid + 1, high);
 
+   mergeArrays(arr, low, mid, high);
+}
+int main() {
 
-int main(){
+   int nums[] = {4, 1, 3, 9, 7};
+   int n = sizeof(nums) / sizeof(nums[0]);
+   int low = 0;
 
+   inversion(nums, low, n - 1);
 
-
-       
-
-
-        return 0;
-    }
+   return 0;
+}
